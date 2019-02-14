419f00578509fb91ea73aed095604d58c2996801
Heikki <19744083+HegePI@users.noreply.github.com>
2019-02-14 16:20:35 +0200

    Osa 4 teht. 4.2 final

--- osa4/Blogilista/index.js
+++ osa4/Blogilista/index.js
@@ -1,41 +1,11 @@
-require('dotenv').config()
+const config = require('./utils/config')
 const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const Blog = require('./models/blog')
+const app = require('./app')
 
-app.use(cors())
-app.use(bodyParser.json())
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
+    console.log(`Server running on port ${config.PORT}`)
 })
 
-app.post('/api/blogs', (request, response) => {
-    const body = request.body
-
-    const newBlog = new Blog({
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes
-
-    })
-
-    newBlog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
