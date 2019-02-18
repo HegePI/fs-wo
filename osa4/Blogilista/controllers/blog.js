b99bf0bfbd3fee30ec13456a4942944e428a9302
Heikki <19744083+HegePI@users.noreply.github.com>
2019-02-18 22:19:43 +0200

    Osa4 teht. 4.18 - 4.19

--- osa4/Blogilista/controllers/blog.js
+++ osa4/Blogilista/controllers/blog.js
@@ -1,6 +1,15 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+        return authorization.substring(7)
+    }
+    return null
+}
 
 
 
@@ -13,22 +22,25 @@ blogRouter.get('/', async (request, response) => {
 blogRouter.post('/', async (request, response, next) => {
     const body = request.body
 
-    console.log(body.user)
-
-    const user = await User.findById(body.user)
-
-    console.log(user)
-
-    const newBlog = new Blog({
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes,
-        user: user._id
-
-    })
+    const token = getTokenFrom(request)
 
     try {
+        const decodedToken = jwt.verify(token, process.env.SECRET)
+        if (!token || !decodedToken.id) {
+            return response.status(401).json({ error: 'token missing or invalid' });
+        }
+
+        const user = await User.findById(decodedToken.id)
+
+        const newBlog = new Blog({
+            title: body.title,
+            author: body.author,
+            url: body.url,
+            likes: body.likes,
+            user: user._id
+
+        })
+
         const savedBlog = await newBlog.save()
         user.blogs = user.blogs.concat(savedBlog._id)
         await user.save()
