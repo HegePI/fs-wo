274abb5d1e541f956e55bdc72d801b6202d1baaf
Heikki <19744083+HegePI@users.noreply.github.com>
2019-02-21 16:21:23 +0200

    Blogi backend samaan kansioon

---
+++ osa5/bloglist_backend/app.js
@@ -0,0 +1,39 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+//const cors = require('cors')
+const Blog = require('./models/blog')
+const blogRouter = require('./controllers/blog')
+const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+
+//app.use(cors())
+app.use(bodyParser.json())
+//app.use('/api/blogs', blogRouter)
+
+console.log('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+    .then(() => {
+        console.log('Connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('Error connecting to MongoDB: ', error.message)
+    })
+
+app.use(bodyParser.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogRouter)
+app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+
+
+module.exports = app
